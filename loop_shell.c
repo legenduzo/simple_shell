eb3d27c48729b02495246bfaf8998377580c1d04
Precious Aleaji <legenduz15@gmail.com>
2023-09-18 06:07:15 +0100

    improved loop_shell

--- loop_shell.c
+++ loop_shell.c
@@ -10,19 +10,22 @@ void shell_loop(char *envp[])
 {
 	char buffer[BUFFER_SIZE];
 	char *read_line;
+	int is_interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-		prompt();
+		if (is_interactive)
+			write(1, "$ ", _strlen("$ "));
 
 		read_line = fgets(buffer, BUFFER_SIZE, stdin);
 		if (!read_line
 				|| !_strncmp(buffer, "exit", 4) || spacescheck(buffer) == _strlen(buffer))
 		{
-			if (read_line == NULL)
+			if (is_interactive && read_line == NULL)
 				write(1, "\n", 1);
 			exit(0);
 		}
+
 		if (!_strncmp(buffer, "env", 3))
 		{
 			_envvar();
