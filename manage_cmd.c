cfeb3197ad105c2081d3c0763fa1c6e38b4f28b4
Salah Eddine <shen.real@live.fr>
2023-09-20 02:35:49 -1200

    fixed betty

--- manage_cmd.c
+++ manage_cmd.c
@@ -1,6 +1,37 @@
 #include "shell.h"
 
 /**
+ *print_error - to print an error and exit the program
+ *@exit_code: the exit code
+ */
+
+void print_error(int exit_code)
+{
+	perror("hsh");
+	exit(exit_code);
+}
+
+/**
+ * **helper_func - make sure we have the good argv
+ * @argv: argument vector coming from main function
+ * Return: it will return the argv that we have checked
+ */
+
+char **helper_func(char **argv)
+{
+	if (!argv)
+		print_error(1);
+
+	if (!dirr(argv[0]))
+		argv[0] = where(argv[0]);
+
+	if (!argv[0])
+		print_error(1);
+
+	return (argv);
+}
+
+/**
  * cmd_manage - handle user input and execute commands
  * @buffer: input from user
  * @envp: envirement pointer
@@ -14,36 +45,18 @@ int cmd_manage(char *buffer, char **envp)
 	char **argv;
 
 	argv = split_string(buffer, ' ');
-
-	if (!argv)
-	{
-		perror("hsh");
-		exit(1);
-	}
-
-	if (!dirr(argv[0]))
-		argv[0] = where(argv[0]);
-
-	if (!argv[0])
-	{
-		perror("hsh");
-		exit(1);
-	}
-
+	argv = helper_func(argv);
 	pid = fork();
 	if (pid == 0)
 	{
 		execve(argv[0], argv, envp);
-		perror("hsh");
-		exit(2);
+		print_error(2);
 	}
 	else if (pid > 0)
 		wait(&status);
 	else
-	{
-		perror("hsh");
-		return (-1);
-	}
+		print_error(-1);
+
 	if (argv)
 	{
 		while (argv[i])
